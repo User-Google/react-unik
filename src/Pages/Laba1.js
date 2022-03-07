7692025eaac129243f35ae9016e5885fe0c124d5
User-Google <swift2plus32@gmail.com>
2022-03-07 15:50:45 +0400

    Added lab1

--- src/Pages/Laba1.js
+++ src/Pages/Laba1.js
@@ -1,74 +1,56 @@
-import { React, useState } from "react";
-import data from '../Data/airportsData.json';
-import { Radio, Select, Button, Modal } from 'antd';
-const { Option } = Select;
+import { useState } from "react";
+import React from "react";
+import {Button} from "antd";
+import { ReloadOutlined } from '@ant-design/icons';
 
+const colors = [ 'green', 'blue', 'red' ]
 
+var interval
 
 function Laba1() {
-    const [country, setCountry] = useState("rusAirport");
-    const [isModalVisible, setIsModalVisible] = useState(false);
-    const [valueOfSelect, setValueOfSelect] = useState('Выберите аэропорт...');
-    function showModal() {
-    setIsModalVisible(true);
-    };
-    const handleCancel = () => {
-    setIsModalVisible(false);
-    };
-  const onChange = e => {
-    setCountry(e.target.value);
-    setValueOfSelect('Выберите аэропорт...');
-  };
-  function handleChange(value) {
-    setValueOfSelect(value);
+    const [value, setValue] = useState('');
+
+  function onButtonClick(e) {
+      setValue(colors.indexOf(e.target.style.background))
+      if (interval){
+          clearInterval(interval)
+      }
+      interval = setInterval(() => {
+          setValue((v) => (v === 2 ? 0 : v + 1));
+      }, 2000)
   }
+
+  function resetColor() {
+      setValue('')
+      if (interval){
+          clearInterval(interval)
+      }
+  }
+
     return (
-        <div className = "wrapper">
-            <div className = "content">
+        <div className ={"wrapper"} style={{ backgroundColor: colors[value] }}>
+            <div className ={"content"}>
                 <h1>Лабораторная работа №1</h1>
                 <div className = "taskConteiner">
                     <h2>Задание:</h2>
                     <p>
-                    &nbsp;Поместите на форму радиокнопки и выпадающий список. Выбор радиокнопки формирует
-                    значения в списке. Например, радионопки могут указывать страны, а в выпадающем списке
-                    будут присутствовать международные аэропорты данных стран.
+                    &nbsp;Поместите на форму разноцветные кнопки. Щелчок по кнопке делает цвет фона
+                        документа таким же, как у данной кнопки. Через 2 секунды цвет фона автоматически
+                        переключается на цвет следующей кнопки и т.д.
                     </p>
                 </div>
-                <div className = "solutionConteiner"
-                //  style = {{display: 'flex'}}
-                 >
+                <div className = "solutionConteiner">
                     <h2>Решение:</h2>
-                    <Radio.Group onChange={onChange} value={country} style = {{display: 'flex', flexDirection: 'row'}}>
-                        <Radio value={'rusAirport'}>Россия</Radio>
-                        <Radio value={'gbrAirport'}>Великобритания</Radio>
-                        <Radio value={'usaAirport'}>Америка</Radio>
-                    </Radio.Group>
-                    <div style = {{marginTop: '10px'}}>
-                    <Select 
-                    value = {valueOfSelect} 
-                    style={{ width: 200, borderRadius: '0px 10px' }} 
-                    onChange={handleChange}
-                    dropdownStyle={{borderRadius: '0px 10px' }}>
-                        {data[country].map(item => {
-                            return (
-                                <Option  key={item.value} value={item.title}>{item.title}</Option>
-                            )
-                        })}
-                    </Select>
-                    <Button type="primary" onClick={showModal} style = {{marginLeft: '30px'}}>Показать JSON</Button>
-                    </div>
-                    <Modal 
-                        visible={isModalVisible} 
-                        onOk={handleCancel} 
-                        okText = "Закрыть" 
-                        cancelButtonProps={{hidden: true}}
-                    >
-                        <pre>
-                        <code>
-                            {JSON.stringify(data, null, 3)}
-                        </code>
-                        </pre>
-                    </Modal>
+                    <button className={'colorButton'} type="primary" onClick={onButtonClick} style = {{
+                        background: 'green',
+                    }}>Зеленый</button>
+                    <button className={'colorButton'} type="primary" onClick={onButtonClick} style = {{
+                        background: 'blue',
+                    }}>Синий</button>
+                    <button className={'colorButton'} type="primary" onClick={onButtonClick} style = {{
+                        background: 'red',
+                    }}>Красный</button>
+                    <Button type="primary" shape="circle" icon={<ReloadOutlined />} onClick={resetColor} />
                 </div>
             </div>
         </div>
