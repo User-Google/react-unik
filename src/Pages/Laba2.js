c779f9d91c6c69c87fa85a6cbe85ef33fc36f074
User-Google <swift2plus32@gmail.com>
2022-03-07 16:43:57 +0400

    Added lab2

--- src/Pages/Laba2.js
+++ src/Pages/Laba2.js
@@ -1,24 +1,11 @@
 import {React, useState} from 'react'
-import minivan from '../Data/minivan.png'
-import pickup from '../Data/pickup.png'
-import sportcar from '../Data/sportcar.png'
+import nightTime from '../Data/night.png'
+import dayTime from '../Data/day.png'
 
 function Laba2() {
     const[mvStyle, setmvStyle] = useState('mvStyle')
     const[puStyle, setpuStyle] = useState('puStyle')
-    const[scStyle, setscStyle] = useState('scStyle')
-    function clickOnMinivan() {
-        console.log('Minivan')
-        mvStyle == 'mvStyle' ? setmvStyle('mvStyle1') : setmvStyle('mvStyle')
-    }
-    function clickOnPickup() {
-        console.log('Pickup')
-        puStyle == 'puStyle' ? setpuStyle('puStyle1') : setpuStyle('puStyle')
-    }
-    function clickOnSportcar() {
-        console.log('Sportcar')
-        scStyle == 'scStyle' ? setscStyle('scStyle1') : setscStyle('scStyle')
-    }
+
     return (
         <div className = "wrapper">
             <div className = "content">
@@ -26,23 +13,18 @@ function Laba2() {
                 <div className = "taskConteiner">
                     <h2>Задание:</h2>
                     <p>
-                    &nbsp;Поместите в документ три рисунка (например, мышка, кошка и собака). Щелчок по
-                    картинке «мышка» заставляет ее убежать, щелчок по «кошке» - мышка убегает, кошка
-                    догоняет, щелчок по «собаке» - собака бежит за кошкой, кошка — за мышкой.
+                    &nbsp;Поместите в документ две картинки (например, день и ночь). Сначала видна только
+                        картинка «день», а картинка «ночь» под ней спрятана. Наведение мыши на картинку
+                        заставляет «день» постепенно становиться прозрачным, а «ночь» — непрозрачной. Когда
+                        мышку убираем с картинки, «ночь» постепенно становится прозрачной, «день» постепенно
+                        становится непрозрачным
                     </p>
                 </div>
                 <div className = "solutionConteiner">
                     <h2>Решение:</h2>
                     <div className='imageConteiner'>
-                        <div className = "carImage">
-                            <img className = {mvStyle} src = {minivan}  height="100" alt="minivan" onClick = {clickOnMinivan}/>
-                        </div>
-                        <div className = "carImage">
-                            <img className = {puStyle} src = {pickup}  height="100" alt="pickup" onClick = {clickOnPickup}/>
-                        </div>
-                        <div className = "carImage">
-                            <img className = {scStyle} src = {sportcar} height="100" alt="sportcar" onClick = {clickOnSportcar}/>
-                        </div>
+                        <img className = {'nightTime'} src = {nightTime}  height="300" alt="nightTime"/>
+                        <img className = {'dayTime'} src = {dayTime} alt="dayTime"/>
                     </div>
                 </div>
             </div>
